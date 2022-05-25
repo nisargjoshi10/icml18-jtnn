de561cc1a7387b56885908ee070ec187d252d638
nisargjoshi10 <nisargj@uw.edu>
2022-05-25 14:10:20 -0700

    mpn

--- fast_jtnn/get_params.py
+++ fast_jtnn/get_params.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 import jtnn_vae
 from jtnn_enc import JTNNEncoder
 from jtnn_dec import JTNNDecoder
+from jtmpn import JTMPN
+from mpn import MPN
 import argparse
 import sys
 sys.path.append('../')
@@ -48,6 +50,20 @@ print(enc)
 
 print('encoder pararms:')
 print("Model #Params: %dK" % (sum([x.nelement() for x in enc.parameters()]) / 1000,))
+
+jtmpn = JTMPN(args.hidden_size, args.depthG)
+print('jtmpn')
+print(jtmpn)
+
+print('jtmpn pararms:')
+print("Model #Params: %dK" % (sum([x.nelement() for x in jtmpn.parameters()]) / 1000,))
+
+mpn = MPN(args.hidden_size, args.depthG)
+print('mpn')
+print(mpn)
+
+print('mpn params:')
+print("Model #Params: %dK" % (sum([x.nelement() for x in mpn.parameters()]) / 1000,))
 
 dec = JTNNDecoder(vocab, args.hidden_size, args.latent_size, nn.Embedding(vocab.size(), args.hidden_size))
 print('decoder')
